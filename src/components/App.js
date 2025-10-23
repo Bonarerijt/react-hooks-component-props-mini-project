c9ffc2044ef74da932d96c3a19e0aa128e36705b
Bonarerijt <prudyjj4@gmail.com>
2025-10-24 02:25:31 +0300

    Completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
 import React from "react";
+import Header from "./Header";
+import About from "./About";
 import blogData from "../data/blog";
+import ArticleList from "./ArticleList";
+import Article from "./Article";
 
 console.log(blogData);
 
@@ -8,6 +12,13 @@ function App() {
     <div className="App">
       You're on your own from here! Follow the deliverables; test things out in
       the browser as you write your code; and good luck!
+      <Header>{blogData.name}</Header>
+      <About>{blogData.image} {blogData.about}</About>
+      <ArticleList posts={blogData.posts}>
+        {blogData.posts.map((post, index) => (
+          <Article key={index} post={post} />
+        ))}
+      </ArticleList>
     </div>
   );
 }
